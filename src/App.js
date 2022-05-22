42091b76b13d3acc5a1fb603cf6e4c0762f32fb0
Michel Amorim <michelmsa7799@gmail.com>
2022-05-21 22:20:33 -0300

    feat: useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
 
-  const reverseClass = reverse ? 'reverse' : ''; // função ternaria
+  // componentDidUpdate - executa toda vez que o component atualiza
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
 
-  const handleClick = () => {
-    setReverse(!reverse);
-    setCounter(counter + 1);
-  };
+  // componentDidMout - executa 1x atualiza
+  useEffect(() => {
+    console.log('componentDidMout');
+  }, []);
+
+  // Com dependencia - executa toda vez que a dependecia mudar
+  useEffect(() => {
+    console.log('contador mudou para', counter);
+  }, [counter]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h1>Contador: {counter}</h1>
-        <button type="button" onClick={handleClick}>
-          Reverse {reverseClass}
-        </button>
-      </header>
+      <h1>Contador: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>push</button>
     </div>
   );
 }
