79ecbeb2d96e90aced47539a2695e83b86bbd0f9
Michel Amorim <michelmsa7799@gmail.com>
2022-05-23 23:08:40 -0300

    feat: useEffect - React Hook

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
+const eventFn = () => {
+  console.log('h1 clicado');
+};
+
 function App() {
   const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
 
-  // componentDidUpdate - executa toda vez que o component atualiza
   useEffect(() => {
-    console.log('componentDidUpdate');
-  });
+    document.querySelector('h1')?.addEventListener('click', eventFn);
 
-  // componentDidMout - executa 1x atualiza
-  useEffect(() => {
-    console.log('componentDidMout');
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
   }, []);
 
   // Com dependencia - executa toda vez que a dependecia mudar
   useEffect(() => {
-    console.log('contador mudou para', counter);
-  }, [counter]);
+    console.log('C1', counter, 'C2', counter2);
+  }, [counter, counter2]);
 
   return (
     <div className="App">
-      <h1>Contador: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>push</button>
+      <h1>
+        C1: {counter} C2: {counter2}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>(+)</button>
     </div>
   );
 }
